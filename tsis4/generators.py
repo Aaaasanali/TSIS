97ba201c93f388d670e6feeae07faedac2de934c
Aaaasanali <156983160+Aaaasanali@users.noreply.github.com>
2024-02-15 10:44:53 +0600

    Create generators.py

---
+++ tsis4/generators.py
@@ -0,0 +1,18 @@
+# N = 4
+# gen = [i**2 for i in range(N)]
+# print(gen)
+
+#------------------------------------------------------
+# n = int(input())
+# gen = [str(i) for i in range(n) if i%2 == 0]
+# print(', '.join(gen))
+
+#------------------------------------------------------
+# def generator(n):
+#     gen = [i for i in range(0, n) if i%3 == 0 or i%4 == 0]
+#     return gen
+
+# print(generator(12))
+
+#------------------------------------------------------
+squares = [for ]
