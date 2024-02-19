09e36aaebd2d7c9959d9a6f4bbf0d05bc21eea34
Aaaasanali <156983160+Aaaasanali@users.noreply.github.com>
2024-02-19 22:25:57 +0600

    Update generators.py

--- tsis4/generators.py
+++ tsis4/generators.py
@@ -15,4 +15,15 @@
 # print(generator(12))
 
 #------------------------------------------------------
-squares = [for ]
+# def squares(a, b):
+#     for i in range(a, b+1):
+#         yield i**2
+
+# for num in squares(1, 5):
+#     print(num)
+
+#------------------------------------------------------
+# n = 10
+
+# a = [i for i in range(10, -1, -1)]
+# print(a)
